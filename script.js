7ef7abf227decd6c53ee75e8b730cf11cd3b2589
SARAH <sarah@alMacBook-Pro-alkhas-bsart.local>
2023-07-17 21:49:35 +0300

    Adding Answer Check button

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-//https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple
+//API URL: https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple
 
-const _question = document.getElementById('question');
-const _options = document.querySelector('.quiz-options');
+
+const _checkBtn = document.getElementById('check-answer');
+
+
+function eventListeners(){
+    _checkBtn.addeventListener('click',checkAnswer)
+}
+
+
 
 
 async function loadQuestions(){
-    const APIUrl = 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple';
+    const APIUrl = 'https://opentdb.com/api.php?amount=20&category=18&difficulty=easy&type=multiple';
     const result = await fetch(`${APIUrl}`);
     const data = await result.json();
     showQuestions(data.results[0]);
+    console.log('hello');
+    console.log('results');
 }
 
 
 function showQuestions(data){
     let correctAnswer = data.correct_answer;
     let incorrectAnswer = data.incorrect_answers;
-    
+
     let optionsList = incorrectAnswer;
     optionsList.splice(Math.floor(Math.random()*(incorrectAnswer.length + 1)),0 , correctAnswer);
 
@@ -23,7 +32,39 @@ function showQuestions(data){
     const _options = document.querySelector('.quiz-options');
 
     document.querySelector('.quiz-options').innerHTML = `${optionsList.map((option,index) =>`
-    <button>${index+1}. <span>${option} </span> </button>`).join('')}`
+    <li>${index+1}. <span>${option} </span> </li>`).join('')}`;
+    //selectOption();
+
 }
 
-loadQuestions();
+// document.querySelector('.quiz-options').innerHTML.querySelectorAll('li')
+// .forEach((option)=>{
+//     option.addeventListener('click', checkAnswer);
+    
+    
+
+
+function selectOption(){
+    document.querySelector('.quiz-options').innerHTML.querySelectorAll('li')
+    .forEach((option)=>{
+        option.addeventListener('click', () => {
+            if (document.querySelector('.quiz-options').querySelector('.selected')){
+                const activeOption = document.querySelector('.quiz-options').querySelector('.selected');
+                activeOption.classList.remove('.selected');
+            }
+            option.classList.add('.selected');
+        })
+    })
+}
+
+function checkAnswer(){
+console.log('hello');
+}
+
+
+// function startQuiz {
+//     currentQuestionIndex =0;
+//     score = 0;
+//     nextButton.innerHTML = "Next";
+//     loadQuestions();
+// }
